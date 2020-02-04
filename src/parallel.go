5d4485b599a3dbfbff02cd6e72d8d2c27ca2001c
I300455 <anton.efremov@sap.com>
2020-02-04 16:39:03 +1100

    Add exec duration to log output

--- src/parallel.go
+++ src/parallel.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
 )
@@ -9,10 +12,15 @@ func runCalcParallel(stub shim.ChaincodeStubInterface, args []string) peer.Respo
 	var resAsBytes []byte
 	var hash1, hash2 string
 
+	start := time.Now()
+
 	for _, val := range args {
 		hash1 = getSingleHashParallel(val)
 		hash2 = getMultiHashParallel(hash1)
 	}
+
+	end := time.Since(start)
+	fmt.Printf("Code execution time for 'runCalcParallel' is %v\n", end)
 
 	resAsBytes = []byte(hash2)
 
